35bf19d78c81fe0336beee4177328f8d814b5ab1
Eyas Wannous <eyaswannous@gmail.com>
2024-10-18 12:44:49 +0300

    Add All

---
+++ lib/main.dart
@@ -0,0 +1,99 @@
+import 'package:flex_color_scheme/flex_color_scheme.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:go_router/go_router.dart';
+import 'package:googleapis_auth/googleapis_auth.dart';
+import 'package:provider/provider.dart';
+
+import 'src/adaptive_login.dart';
+import 'src/adaptive_playlists.dart';
+import 'src/app_state.dart';
+import 'src/playlist_details.dart';
+
+// https://developers.google.com/youtube/v3/guides/auth/installed-apps#identify-access-scopes
+final scopes = [
+  'https://www.googleapis.com/auth/youtube.readonly',
+];
+
+final clientId = ClientId(
+  dotenv.env['CLIENT_ID']!,
+  dotenv.env['CLIENT_SECRET']!,
+);
+
+final _router = GoRouter(
+  routes: <RouteBase>[
+    GoRoute(
+      path: '/',
+      builder: (context, state) {
+        return const AdaptivePlaylists();
+      },
+      redirect: (context, state) {
+        if (!context.read<AuthedUserPlaylists>().isLoggedIn) {
+          return '/login';
+        } else {
+          return null;
+        }
+      },
+      routes: <RouteBase>[
+        GoRoute(
+          path: 'login',
+          builder: (context, state) {
+            return AdaptiveLogin(
+              clientId: clientId,
+              scopes: scopes,
+              loginButtonChild: const Text('Login to YouTube'),
+            );
+          },
+        ),
+        GoRoute(
+          path: 'playlist/:id',
+          builder: (context, state) {
+            final title = state.uri.queryParameters['title']!;
+            final id = state.pathParameters['id']!;
+            return Scaffold(
+              appBar: AppBar(title: Text(title)),
+              body: PlaylistDetails(
+                playlistId: id,
+                playlistName: title,
+              ),
+            );
+          },
+        ),
+      ],
+    ),
+  ],
+);
+
+Future<void> main() async {
+  // Load environment variables
+  await dotenv.load(fileName: ".env");
+
+  runApp(
+    ChangeNotifierProvider<AuthedUserPlaylists>(
+      create: (context) => AuthedUserPlaylists(),
+      child: const PlaylistsApp(),
+    ),
+  );
+}
+
+class PlaylistsApp extends StatelessWidget {
+  const PlaylistsApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp.router(
+      title: 'Your Playlists',
+      theme: FlexColorScheme.light(
+        scheme: FlexScheme.red,
+        useMaterial3: true,
+      ).toTheme,
+      darkTheme: FlexColorScheme.dark(
+        scheme: FlexScheme.red,
+        useMaterial3: true,
+      ).toTheme,
+      themeMode: ThemeMode.system,
+      debugShowCheckedModeBanner: false,
+      routerConfig: _router,
+    );
+  }
+}
